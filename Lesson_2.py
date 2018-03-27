e9de2c243b4a1c1522db958acfcc0922da3285cd
Adam Kahn <adamkahn@ad.corp.google.com>
2018-03-26 17:11:27 -0700

    Change lesson note to #2: use function

--- Lesson_2.py
+++ Lesson_2.py
@@ -2,5 +2,5 @@
 # Corresponds to the requirements of Udacity course "Programming Foundations with Python"
 # https://www.udacity.com/course/programming-foundations-with-python--ud036
 
-# Lesson 1: Introductions
+# Lesson 2: Use Functions
 
