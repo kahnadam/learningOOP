30b5cebd466f8208231d067b0593a0384dc75c39
Adam Kahn <adamkahn@ad.corp.google.com>
2018-03-26 17:09:35 -0700

    Add lesson_2 practice file

---
+++ Lesson_2.py
@@ -0,0 +1,6 @@
+# Setting up a file to practice coding in Python
+# Corresponds to the requirements of Udacity course "Programming Foundations with Python"
+# https://www.udacity.com/course/programming-foundations-with-python--ud036
+
+# Lesson 1: Introductions
+
