653dca0bc7a6b8199779eba4032e29048b8ce298
Adam Kahn <adamkahn@ad.corp.google.com>
2018-03-30 14:31:25 -0700

    Add loop to repeat breaks X times

--- Lesson_2.py
+++ Lesson_2.py
@@ -7,7 +7,13 @@
 import time
 import webbrowser
 
-time.sleep(4)
-webbrowser.open("http://youtube.com")
+total_breaks = 3
+break_count = 0
+
+print("This program started on "+time.ctime())
+while (break_count < total_breaks): 
+	time.sleep(4)
+	webbrowser.open("http://youtube.com")
+	break_count = break_count + 1
 
 
