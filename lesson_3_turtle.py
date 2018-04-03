22250dc50164c52a505b05b1b92e48387de5e777
Adam Kahn <adamkahn@ad.corp.google.com>
2018-04-03 15:25:35 -0700

    Make Brad turn four times to make a square

--- lesson_3_turtle.py
+++ lesson_3_turtle.py
@@ -3,8 +3,18 @@ import turtle
 def draw_square():
     window = turtle.Screen() #make a window
     window.bgcolor("red") #window color is red
+    
     brad = turtle.Turtle() #define brad as the turtle
+
     brad.forward(100) #move line forward 100
+    brad.right(90) #turn 90 degrees
+    brad.forward(100) #move line forward 100
+    brad.right(90) #turn 90 degrees
+    brad.forward(100) #move line forward 100
+    brad.right(90) #turn 90 degrees
+    brad.forward(100) #move line forward 100
+    brad.right(90) #turn 90 degrees
+
     window.exitonclick() #close the window when you click on it
 
 draw_square()
