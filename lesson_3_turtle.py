1a6eec52d19a82c47d031c919ec1e62dae637c13
Adam Kahn <adamkahn@ad.corp.google.com>
2018-04-03 15:32:23 -0700

    Add a second turtle that makes a circle

--- lesson_3_turtle.py
+++ lesson_3_turtle.py
@@ -4,7 +4,7 @@ def draw_square():
     window = turtle.Screen() #make a window
     window.bgcolor("red") #window color is red
     
-    brad = turtle.Turtle() #define brad as the turtle
+    brad = turtle.Turtle() #create an instance of a class called turtle
     brad.shape("turtle") #make brad turtle shaped
     brad.color("yellow") #make brad yellow
     brad.speed(2) #change brad speed to 2
@@ -17,6 +17,11 @@ def draw_square():
     brad.right(90) #turn 90 degrees
     brad.forward(100) #move line forward 100
     brad.right(90) #turn 90 degrees
+
+    angie = turtle.Turtle() #make a second turtle
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
 
     window.exitonclick() #close the window when you click on it
 
