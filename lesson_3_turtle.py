8e7c0237adca9fdfcbaf8b28614b485b89b3c33c
Adam Kahn <adamkahn@ad.corp.google.com>
2018-04-03 15:27:23 -0700

    Change shape, color, speed of Brad the turtle

--- lesson_3_turtle.py
+++ lesson_3_turtle.py
@@ -5,6 +5,9 @@ def draw_square():
     window.bgcolor("red") #window color is red
     
     brad = turtle.Turtle() #define brad as the turtle
+    brad.shape("turtle") #make brad turtle shaped
+    brad.color("yellow") #make brad yellow
+    brad.speed(2) #change brad speed to 2
 
     brad.forward(100) #move line forward 100
     brad.right(90) #turn 90 degrees
