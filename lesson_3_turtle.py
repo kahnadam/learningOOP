4fb7eebecd6d8911d8645e57aacb870e975dbdc3
Adam Kahn <adamkahn@ad.corp.google.com>
2018-04-03 15:43:13 -0700

    Refactor code to create a window and then draw all art

--- lesson_3_turtle.py
+++ lesson_3_turtle.py
@@ -1,28 +1,27 @@
 import turtle
 
-def draw_square():
+
+def draw_square(some_turtle):
+    for i in range (1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():
     window = turtle.Screen() #make a window
     window.bgcolor("red") #window color is red
-    
+
+    #make Brad
     brad = turtle.Turtle() #create an instance of a class called turtle
     brad.shape("turtle") #make brad turtle shaped
     brad.color("yellow") #make brad yellow
     brad.speed(2) #change brad speed to 2
-
-    brad.forward(100) #move line forward 100
-    brad.right(90) #turn 90 degrees
-    brad.forward(100) #move line forward 100
-    brad.right(90) #turn 90 degrees
-    brad.forward(100) #move line forward 100
-    brad.right(90) #turn 90 degrees
-    brad.forward(100) #move line forward 100
-    brad.right(90) #turn 90 degrees
-
+    draw_square(brad)
+    #make Angie
     angie = turtle.Turtle() #make a second turtle
     angie.shape("arrow")
     angie.color("blue")
     angie.circle(100)
 
-    window.exitonclick() #close the window when you click on it
+    window.exitonclick()
 
-draw_square()
+draw_art()
