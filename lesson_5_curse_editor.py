e6e08688e4daf26ee16ba463380e24f4ac80fbbf
Adam Kahn <adamkahn@ad.corp.google.com>
2018-04-04 16:28:47 -0700

    Add function to check text for profanity

--- lesson_5_curse_editor.py
+++ lesson_5_curse_editor.py
@@ -1,7 +1,17 @@
+import urllib
+
 # open the file of text
 def read_text():
     quotes = open(r"C:\Users\adamkahn\learningOOP\quotes.txt")
     contents_of_file = quotes.read() #allows you to read the contents of the file
     print(contents_of_file)
     quotes.close() #close the file that we opened
+    check_profanity(contents_of_file)
+
+def check_profanity(text_to_check):
+    connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+text_to_check) #opens a connection to a website
+    output = connection.read()
+    print(output)
+    connection.close()
+    
 read_text()
