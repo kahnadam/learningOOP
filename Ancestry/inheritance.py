96eb737d134b528d78b1c70f1e94de5ac7a9f5e4
Adam Kahn <adamkahn@ad.corp.google.com>
2018-04-09 15:04:39 -0700

    Create new class Child which inherits from class Parent

--- Ancestry/inheritance.py
+++ Ancestry/inheritance.py
@@ -3,3 +3,9 @@ class Parent():
         print("Parent Constructor Called")
         self.last_name = last_name
         self.eye_color = eye_color
+
+class Child(Parent):
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child Constructor Called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
