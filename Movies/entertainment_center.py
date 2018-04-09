5887a1796596040696ecaefcf7a6ba3051017d8e
Adam Kahn <adamkahn@ad.corp.google.com>
2018-04-08 18:03:31 -0700

    Create object toy_story

---
+++ Movies/entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://goo.gl/gW6G54",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc") 
+
+print(toy_story.storyline)
