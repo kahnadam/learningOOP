d7e00f499a08b8d36c3fedce685c23a1a4addb01
Adam Kahn <adamkahn@ad.corp.google.com>
2018-04-08 18:31:19 -0700

    Add four new objects - airplane, school_of_rock, 12_angry_men, superbad

--- Movies/entertainment_center.py
+++ Movies/entertainment_center.py
@@ -5,11 +5,34 @@ toy_story = media.Movie("Toy Story",
                         "https://goo.gl/gW6G54",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc") 
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
                      "http://www.impawards.com/2009/posters/avatar.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-print(avatar.storyline)
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+airplane = media.Movie("Airplane!",
+                       "Comedic mishaps as a plane's pilots fall ill",
+                       "http://img.moviepostershop.com/airplane-movie-poster-1980-1020196787.jpg",
+                       "https://www.youtube.com/watch?v=rzRJWy-3_Dc")
+#airplane.show_trailer()
+
+school_of_rock = media.Movie("School of Rock",
+                             "Using rock music to learn",
+                             "http://img.moviepostershop.com/the-school-of-rock-movie-poster-2003-1020191888.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+12_angry_men = media.Movie("12 Angry Men",
+                           "The debates of a murder trial's jury",
+                           "https://images-na.ssl-images-amazon.com/images/I/81LX%2BJlavLL._SY550_.jpg",
+                           "https://www.youtube.com/watch?v=A7CBKT0PWFA")
+
+superbad = media.Movie("Superbad",
+                       "High school kids try to get booze and party",
+                       "https://images-na.ssl-images-amazon.com/images/I/51gr0eN7X0L.jpg",
+                       "https://www.youtube.com/watch?v=MNpoTxeydiY")
+
